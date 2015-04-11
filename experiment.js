1b153404a086df46be3c438d019a1fad45c185f6
samzhang111 <shimian.zhang@gmail.com>
2015-04-11 18:31:53 -0400

    Preliminary prototype for mouseover
    - Redraws bounding box on mouseover and mouseout
    - Hitbox is a bit large, so it feels clumsy
    - Sometimes the new divs aren't updated fast enough to be deleted;
        causes console errors.

--- experiment.js
+++ experiment.js
@@ -1,6 +1,10 @@
 (function() {
     // Actual dimensions of a single character
-    var cdims;
+    var cdims,
+        default_box_hfill = "=",
+        default_box_vfill = "|",
+        mouseover_hfill = "~",
+        mouseover_vfill = "*";
 
     document.addEventListener("DOMContentLoaded", function(event) {
         cdims = get_char_dims();
@@ -8,7 +12,6 @@
 
         var to_box = document.getElementsByClassName('ascii-box');
 
-        console.log(to_box);
         for (var i=0; i<to_box.length; i++) {
 
             // Take each element, create div box with abs positioning 
@@ -20,42 +23,87 @@
 
                 // Get structural dimensions of box
                 sdims = get_div_dims(to_box[i]),
-                
-                // Generate bounding box string using structural dims
-                box_text = make_box(sdims.w, sdims.h),
+                bounding_box = redraw(pdims, sdims, default_box_hfill, 
+                            default_box_vfill);
 
-            // Set bounding box to where element is
-            bounding_box = document.createElement('div');
-            bounding_box.className = 'box-outline';
-            bounding_box.style.left = pdims.left - cdims.w + 'px';
-            bounding_box.style.top = pdims.top - 1.9*cdims.h + 'px';
+            // Is the box clickable?
+            if (to_box[i].classList.contains('clickable')) {
+                // If so, set up event handler for mouseovers
+                bounding_box.onmouseover = mouseover_handler;
+            }
 
-            // Construct box based off height and width of element
-            
-            console.log(box_text);
-            bounding_box.innerHTML = box_text;
-
-            document.body.appendChild(bounding_box);
         }
     });
+    function mouseover_handler(e) {
+        var sdims = { h: this.dataset.sdims_h,
+                      w: this.dataset.sdims_w },
+            pdims = { top: this.dataset.pdims_top,
+                      left: this.dataset.pdims_left },
+            parent = this.parentNode;
 
-    function make_box(width, height) {
+        parent.removeChild(this);
+
+        var bbox = redraw(pdims, sdims, mouseover_hfill, 
+                            mouseover_vfill);
+    
+        bbox.style.cursor = "pointer";
+        bbox.onmouseout = mouseout_handler;
+    }
+    
+    function mouseout_handler(e) {
+        var sdims = { h: this.dataset.sdims_h,
+                      w: this.dataset.sdims_w },
+            pdims = { top: this.dataset.pdims_top,
+                      left: this.dataset.pdims_left },
+            parent = this.parentNode;
+
+        parent.removeChild(this);
+        var bbox = redraw(pdims, sdims, default_box_hfill, 
+                            default_box_vfill);
+
+        bbox.style.cursor = "auto";
+        bbox.onmouseover = mouseover_handler;
+    }
+    function redraw(pdims, sdims, box_filler, ends) {
+            
+            // Generate bounding box string using structural dims
+        var box_text = make_box(sdims.w, sdims.h, box_filler, ends),
+            // Set bounding box to where element is
+            bounding_box = document.createElement('div');
+
+        // Generate random ID for the boundary box
+        bounding_box.className = 'box-outline';
+        bounding_box.style.left = pdims.left - cdims.w + 'px';
+        bounding_box.style.top = pdims.top - 1.9*cdims.h + 'px';
+        
+        // Construct box based off height and width of element
+        bounding_box.innerHTML = box_text;
+        bounding_box.dataset.sdims_h = sdims.h;
+        bounding_box.dataset.sdims_w = sdims.w;
+        bounding_box.dataset.pdims_left = pdims.left;
+        bounding_box.dataset.pdims_top = pdims.top;
+
+        document.body.appendChild(bounding_box);
+
+        return bounding_box;
+    }
+
+    function make_box(width, height, filler, ends) {
         var cw = Math.floor(width / cdims.w) + 1,
             ch = Math.floor(height / cdims.h),
             box = "<pre>",
-            row_filler = "=";
+            row_filler = filler;
         
-        console.log(width, height, cdims.w, cdims.h, cw, ch);
         // Make top row
-        box += draw_box_row(row_filler, cw);
+        box += draw_box_row(row_filler, cw, ends);
         
         // Make bounding sides
         for (var j=0; j<ch; j++) {
-            box += draw_box_row("&nbsp;", cw);
+            box += draw_box_row("&nbsp;", cw, ends);
         }
         
         // Make box bottom
-        box += draw_box_row(row_filler, cw);
+        box += draw_box_row(row_filler, cw, ends);
         
         box += "</pre>";
         return box;
@@ -70,14 +118,13 @@
         cdims.h = test_div.clientHeight;
         test_div.innerHTML = '';
         
-        console.log('cdims:', cdims);
         return cdims;
     }
     
     // Draw box row
-    function draw_box_row(filler, width) {
-        var vfill_left = "|",
-            vfill_right = "|";
+    function draw_box_row(filler, width, ends) {
+        var vfill_left = ends,
+            vfill_right = ends;
         var row = "";
 
         row += vfill_left;
